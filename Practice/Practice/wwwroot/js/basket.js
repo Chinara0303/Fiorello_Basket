115dfd01aebe1973259519cfc6766f6d4b4fe167
Chinara0303 <chinaraei@code.edu.az>
2023-04-13 19:57:44 +0400

    Added add basket method with ajax

---
+++ Practice/Practice/wwwroot/js/basket.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".delete-product", function (e) {
+        e.preventDefault();
+        let decodedCookie = decodeURIComponent(document.cookie.indexOf("Id"));
+        console.log(decodedCookie)
+
+        //let c;
+        //for (var i = 0; i < decodedCookie.length; i++) {
+        //   c=decodedCookie[0];
+        //}
+        console.log(c)
+        $.ajax({
+            type: "Get",
+            url: "basket/delete/id",
+            success: function (res) {
+                console.log(res)
+            }
+        })
+
+    })
+})
